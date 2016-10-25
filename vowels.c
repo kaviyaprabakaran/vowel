8ee6fef978f7d37b4f257b1e47089762cd9681bd
kaviyaprabakaran <kaviya595@gmail.com>
2016-10-25 15:07:09 +0530

    Create vowels.c

---
+++ vowels.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+char n;
+printf("enter the alphabet:");
+scanf("%d",&n);
+switch(n)
+{
+    case 'a':
+    printf("it is vowel");
+    break;
+     case 'e':
+    printf("it is vowel");
+    break;
+     case 'i':
+    printf("it is vowel");
+    break;
+     case 'o':
+    printf("it is vowel");
+    break;
+     case 'u':
+    printf("it is vowel");
+    break;
+    default:
+    printf("it is consonant");
+    break;
+}
+
+    return 0;
+    }
